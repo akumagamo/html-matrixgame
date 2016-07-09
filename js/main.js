fa4155255da7c81c1e0fa56108cc77b21b7d7184
Charles Lam <charles.lam@gmx.at>
2016-07-09 08:57:23 +0200

    development

--- js/main.js
+++ js/main.js
@@ -10,44 +10,55 @@ debug("Up and Running!");
 
 
 var game = {
-    init:function(){
-        this.gameElement = document.getElementById("gameBoard");
-        
-       /* var scoreBox = document.createElement("div");
-        scoreBox.className = "score-board";
-        var timerBox = document.createElement("div");
-        timerBox.className = "timer-display";*/
-        var gameBox = document.createElement("div");
-        gameBox.className = "game-box";
+    init: function () {
+        // prepare Engine
+        this.gameState = {
+            newBlock: {
+                position: {
+                    x: 0,
+                    y: 0
+                },
+                value: "a"
+            },
+            gameBoard: {
+                data: ((width, height) => "0".repeat(width).split("").map(x=>"0".repeat(height).split("")))(20, 10),
+                size: {
+                    height: 20,
+                    width: 10
+                }
+            }
+        };
 
-        //this.gameElement.appendChild(scoreBox);
-        //this.gameElement.appendChild(timerBox);
-        this.gameElement.appendChild(gameBox);
-
-        if(!this.currentBlock)
-        {
-            this.currentBlock = document.createElement("div");
-            this.currentBlock.className = "game-block";
-            this.currentBlock.setAttribute("style","top:-20px");
-            gameBox.appendChild(this.currentBlock);
-        }
-                
+        this.currentGame = {
+            isActive: true,
+            score: 0,
+            elapsedTime: 0,
+            destroyedBlocks: 0 
+        };
     },
-    render:function()
-    {
-        
-        this.move();
+    advanceOneStep: function () {
+        debug("in");
+       if(this.willCollide()){
+            // position Block
+            // calculate Blocks
+            // calculate Score
+            // new Block
+       }
+       else{
+           // move Block
+           this.gameState.newBlock.position.y--;
+       }
     },
-    move: function()
-    {
-        var speed = 5;
-        if(this.currentBlock){
-            if(parseInt(this.currentBlock.style.top)+speed < 401-40)
-            this.currentBlock.style.top = (parseInt(this.currentBlock.style.top)+speed) + "px" ;
-        }
+    willCollide: function () {
+        return false;
+    },
+    render: function(){
+        game.advanceOneStep();
+        debug(game.gameState.newBlock.position.y);
     }
-
 
 };
 
 game.init();
+
+setInterval(game.render.bind(game),33);
